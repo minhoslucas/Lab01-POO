cc77316f1fb1262f8844f1730fc3840d9fb320a1
minhoslucas <lucasbeserrafernandes@gmail.com>
2025-03-26 00:21:29 -0300

    Histórico de Eventos implementado (ainda não testado)

--- Lab01/app/src/main/java/lab01/HistoricoEventos.java
+++ Lab01/app/src/main/java/lab01/HistoricoEventos.java
@@ -1,5 +1,29 @@
 package lab01;
 
+import java.util.ArrayList;
+
 public class HistoricoEventos {
-    //IMPLEMENTAR
+    private ArrayList<Evento> event_list;
+
+    public HistoricoEventos(ArrayList<Evento> event_list) {
+        this.event_list = event_list;
+    }
+
+    public ArrayList<Evento> getEventList() {
+        return this.event_list;
+    }
+
+    public void setEventList(ArrayList<Evento> event_list) {
+        this.event_list = event_list;
+    }
+
+    public void addToList(Evento event) {
+        event_list.add(event);
+    }
+
+    public void showEventList() {
+        for (int i = 0; i < event_list.size(); i++) {
+            System.out.println(event_list.get(i).getNome());
+        }
+    }
 }
